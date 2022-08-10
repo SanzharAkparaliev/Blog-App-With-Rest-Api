5f1ab15e8465651280743087c2012ca0ac5bdb9f
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-08-10 23:22:39 +0600

    first commit

---
+++ src/test/java/com/spring/blogappapis/BlogAppApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blogappapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

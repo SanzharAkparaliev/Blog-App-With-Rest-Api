c4d45b4b82e3a7a1db0c5d05451bac980190c51d
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-08-11 22:53:58 +0600

    finishing user api

--- src/main/java/com/spring/blogappapis/controllers/UserController.java
+++ src/main/java/com/spring/blogappapis/controllers/UserController.java
@@ -1,11 +1,15 @@
 package com.spring.blogappapis.controllers;
 
+import com.spring.blogappapis.payloads.ApiResponse;
 import com.spring.blogappapis.payloads.UserDto;
 import com.spring.blogappapis.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,5 +23,27 @@ public class UserController {
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{userId}")
+    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto,@PathVariable("userId") Long useeId){
+        UserDto updatedUser = this.userService.updateUser(userDto,useeId);
+        return ResponseEntity.ok(updatedUser);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<ApiResponse> deleteUser(@PathVariable("userId") Long userId){
+        this.userService.deleteUser(userId);
+        return  new ResponseEntity<>(new ApiResponse("User deleted successfully",true),HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<UserDto>> getAllUser(){
+        return ResponseEntity.ok(this.userService.getAllUsers());
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserDto> getSingleUser(@PathVariable("userId") Long userId){
+        return ResponseEntity.ok(this.userService.getUserById(userId));
     }
 }
--- src/main/java/com/spring/blogappapis/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/spring/blogappapis/exceptions/ResourceNotFoundException.java
@@ -9,10 +9,10 @@ import lombok.Setter;
 public class ResourceNotFoundException extends RuntimeException{
     String resourceName;
     String fieldName;
-    Long fieldValue;
+    long fieldValue;
 
-    public ResourceNotFoundException(String resourceName, String fieldName, Long fieldValue) {
-        super(String.format("%s not found with %s : %l",resourceName,fieldName,fieldValue));
+    public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
+        super(String.format("%s not found with %s : %s",resourceName,fieldName,fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;

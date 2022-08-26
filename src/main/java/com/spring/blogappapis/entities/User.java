837e7d03be91e367e50ea54784b5712479485f5f
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-08-26 22:37:46 +0600

    role specific API access

--- src/main/java/com/spring/blogappapis/entities/User.java
+++ src/main/java/com/spring/blogappapis/entities/User.java
@@ -40,8 +40,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        this.roles.stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-        return null;
+        return     this.roles.stream().map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
     }
 
     @Override
